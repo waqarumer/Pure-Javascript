43acde648febb4bd108c431c1819f12f75acf947
waqarumer <waqarkhan864@gmail.com>
2023-12-20 04:15:47 +0500

    bsic variable scope

--- javascript/01_basics/01_variables.js
+++ javascript/01_basics/01_variables.js
@@ -1,4 +1,22 @@
 const acountId = 1234
 let accountEmail = "waqar@gmail.com"
 var accountPassword = "12345"
-accountCity = "lahore"
+accountCity = "lahore" // in javascript also define variable in that way 
+
+let accountState;
+
+// console.log(acountId) 
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+
+
+
+*/
+
+accountEmail = "test123@gmail.com"
+accountPassword = "2121221"
+accountCity = "Gujranwala"
+// console.table([acountId,accountEmail,accountPassword,accountCity]) // to console log in tabular form 
+//console.log(accountState)
