0192484dd6bca9e9da4ddd0429da3b18fd8090cd
waqarumer <waqarkhan864@gmail.com>
2023-12-20 04:30:54 +0500

    datatypes

---
+++ javascript/01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version 
+
+//alert("hello world");
+
+let name = "waqar khan"
+let age = 30
+let isLoggedIn = false
+let state;
+
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+
+
+//object
+
+//console.log(typeof "waqar khan") // to find the type of 
