0c2014648f09e4151fbb42b4e668e91a57d6c5fd
waqarumer <waqarkhan864@gmail.com>
2023-12-21 00:43:18 +0500

    comparison oprators

---
+++ javascript/01_basics/04_comparison.js
@@ -0,0 +1,11 @@
+//comparison oprator must b equal type 
+
+// strict check uses === it also check data type 
+//==
+//--
+//<
+//>
+//<=
+console.log(2 == 1 )
+console.log(2 <= 1 )
+console.log(2 >= 1 )
