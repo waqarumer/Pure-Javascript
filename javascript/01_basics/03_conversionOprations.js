d96241b3fd5952207b224315c518639362bc8c0f
waqarumer <waqarkhan864@gmail.com>
2023-12-20 15:55:18 +0500

    conversion of data types

---
+++ javascript/01_basics/03_conversionOprations.js
@@ -0,0 +1,32 @@
+//let score = "33abc"
+let score = true;
+//console.log(typeof score);
+
+//another way of getting type of
+
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NAN
+// true => 1; false => 0
+
+//let isLoggedIn = 1
+let isLoggedIn = "waqar Khan"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+// "" => false
+//"waqar" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+//console.log(someNumber);
+
+console.log(typeof stringNumber);
