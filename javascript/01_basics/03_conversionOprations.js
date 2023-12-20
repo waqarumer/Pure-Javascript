827336a05ca0da4b9c7a3279a634e2a59ed8aebd
waqarumer <waqarkhan864@gmail.com>
2023-12-20 16:22:18 +0500

    conversion with prefix and postfix oprations

--- javascript/01_basics/03_conversionOprations.js
+++ javascript/01_basics/03_conversionOprations.js
@@ -23,10 +23,52 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // "" => false
 //"waqar" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
+// let stringNumber = String(someNumber)
 
-//console.log(someNumber);
+// //console.log(someNumber);
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ********************oprations*********************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "khan"
+
+let str3 = str1 + str2
+//console.log(str3);
+//console.log("1" + 2);
+//console.log("1" + 2 +2);
+//console.log(2 + 2 +"1");
+
+let gameCounter = 100
+//prefix increment
+//gameCounter++;
+
+//postfix increment
+++gameCounter;
+//console.log(gameCounter);
+
+let x = 3
+let y = x++;
+
+console.log(`x:${x}, y:${y}`);
+
+let a = 3;
+let b = ++a;
+
+console.log(`a:${a},b:${b}`);
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment for knowing prefix and post fix
