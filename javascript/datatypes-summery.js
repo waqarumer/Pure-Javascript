1859d905b426f1ece7b9e56a528f88d38aecab0d
waqarumer <waqarkhan864@gmail.com>
2023-12-22 03:09:33 +0500

    datatype summery

---
+++ javascript/datatypes-summery.js
@@ -0,0 +1,37 @@
+// #premative Data types
+
+// 7 types : String, Numbers, Boolean, null, undefined, Symbol(it is for uniqueness of values), BigInt it is call by values 
+
+const score = 100
+
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id === anotherId)
+
+//const bigNumber = 343234234432234n
+
+//console.log(bigNumber)
+
+// Refrence type or non premetive
+
+// Array, Objects, Functions
+
+const heros = ["spiderman","batman","hitman"];
+
+let myobj = {
+    name: "waqar khan",
+    age: 31
+}
+
+const myFunction = function(){
+        console.log("hello world");
+}
+console.log(anotherId)
+
