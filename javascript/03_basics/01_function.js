5b1eaa117a83cd4ed73a0aabe865a8c6412aee39
waqarumer <waqarkhan864@gmail.com>
2023-12-26 17:48:33 +0500

    function defination with objects and arrays

--- javascript/03_basics/01_function.js
+++ javascript/03_basics/01_function.js
@@ -41,4 +41,39 @@ function loginUserMessage(username){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage());
+//console.log(loginUserMessage());
+
+
+// situation on shoping cart
+// ... is rest oprator 
+
+function calculateCartPrice (...num1){
+
+    return num1
+}
+
+//console.log(calculateCartPrice(200,400,600));
+
+const user = {
+    username: "waqar khan",
+    price: 199
+}
+
+function handleObject(anyobject){
+
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+handleObject(user)
+
+
+const myNewArray = [200,600,900]
+
+function returnSecondValue(getArray){
+
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+
+
+
