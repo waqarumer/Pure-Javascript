a03111db524bf0cbf2043aceb51a4a5d54e9b656
waqarumer <waqarkhan864@gmail.com>
2023-12-26 22:41:45 +0500

    switches and tenery oprators

---
+++ javascript/04_control_flow/one.js
@@ -0,0 +1,40 @@
+// code is run on conditional base 
+
+// following is flow 
+
+// if
+
+// we use comparisons to avaluate the condition 
+
+// if(true){
+
+// }
+
+const isUserloggedIn = false
+
+// if(isUserloggedIn == true) {
+
+//     console.log("User is logged in");
+// } else {
+//     console.log("user is not logged in")
+// }
+
+
+// const temperature = 51
+
+
+// if (temperature < 50) {
+    
+//     console.log("less then 50");
+// } else {
+//     console.log("temperature is grater then 50");
+// }
+
+
+
+// short hand notations 
+
+
+const balance = 2000
+
+if(balance > 500) console.log("test"); // this is short notation write in one line
---
+++ javascript/04_control_flow/switch.js
@@ -0,0 +1,39 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+const month = ""
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+        
+
+
+    default:
+        console.log("default case match");
+        break;
+}
