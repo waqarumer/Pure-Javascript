8289968412ec686e723069f751be68bf8689a7cf
waqarumer <waqarkhan864@gmail.com>
2023-12-26 14:45:57 +0500

    diffrent kind of arrays

--- javascript/02_basics/02_arrays.js
+++ javascript/02_basics/02_arrays.js
@@ -17,4 +17,18 @@ const anotherArray = [1,2,3,[4,5,6],7,[6,7,8,9]]
 
 const real_Array = anotherArray.flat(Infinity)
 
-console.log(real_Array);
+//console.log(real_Array);
+
+console.log(Array.isArray("waqar")); // it is for knowing or questioning is it array or not 
+
+console.log(Array.from("Waqar")); // to convert in array 
+console.log(Array.from({name: "waqar khan"})); // it is interesting to defining array in keys or values if we dont define then it shows empty 
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+
+
+console.log(Array.of(score1,score2,score3));
