794157fc6409ba68d115db08d4617386ece955e2
waqarumer <waqarkhan864@gmail.com>
2024-01-16 03:27:46 +0500

    object

---
+++ javascript/09_advance_one/object.js
@@ -0,0 +1,26 @@
+
+function createUser(username, score){
+    this.username = username
+    this.score = score
+}
+
+createUser.prototype.increment = function(){
+    this.score++
+}
+
+createUser.prototype.printMe = function(){
+    console.log(`price is ${this.score}`);
+}
+const chai = new createUser("chai",25)
+const tea = createUser("tea",250)
+
+
+chai.printMe()
+
+
+// a new object is created: the new keyword initiates the creation of a new javascript object 
+
+
+
+
+
